55233228306917511961405c5ba66c79e8de270b
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2014-03-25 23:04:36 +0100

    PORTO-7 Updated initializer

--- shell/test/force/initialize.js
+++ shell/test/force/initialize.js
@@ -5,5 +5,8 @@ __main__ = function()
 {
     var storage = JSONStorage();
     var particles = Entity.instance('particles', '0.0.1');
-
+    
+    particles.diameter = 100e-6;
+    particles.density  = 1000;
+    storage.commit(particles);
 }
